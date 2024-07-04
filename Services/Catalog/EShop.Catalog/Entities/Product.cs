fbd02a0e430f4d287dcd5fb6477141acbffeaf7d
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-07-04 16:00:23 +0300

    Add Product Entity

---
+++ Services/Catalog/EShop.Catalog/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace EShop.Catalog.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string productId { get; set; }
+        public string productName { get; set; }
+        public decimal productPrice { get; set; }
+        public string productDescription { get; set; }
+        public string productImage { get; set; }
+        public string productCategory { get; set; }
+
+        [BsonIgnore]
+        public Category category { get; set; }
+    }
+}
