1488038602a66a9c62311509fdc732dd913b3e63
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-07-04 17:09:02 +0300

    Add ProductDtos for Create, Update, Result and GetById

---
+++ Services/Catalog/EShop.Catalog/Dtos/ProductDtos/CreateProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace EShop.Catalog.Dtos.ProductDtos
+{
+    public class CreateProductDto
+    {
+        public string productName { get; set; }
+        public decimal productPrice { get; set; }
+        public string productDescription { get; set; }
+        public string productImage { get; set; }
+        public string productCategory { get; set; }
+    }
+}
---
+++ Services/Catalog/EShop.Catalog/Dtos/ProductDtos/GetByIdProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace EShop.Catalog.Dtos.ProductDtos
+{
+    public class GetByIdProductDto
+    {
+        public string productId { get; set; }
+        public string productName { get; set; }
+        public decimal productPrice { get; set; }
+        public string productDescription { get; set; }
+        public string productImage { get; set; }
+        public string productCategory { get; set; }
+    }
+}
