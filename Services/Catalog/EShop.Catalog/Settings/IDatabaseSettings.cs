d38eae70617c27baba514931d12e72173cb7cb0a
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-07-04 16:01:35 +0300

    Add DatabaseSettings

---
+++ Services/Catalog/EShop.Catalog/Settings/IDatabaseSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace EShop.Catalog.Settings
+{
+    public interface IDatabaseSettings
+    {
+        public string categoryCollectionName { get; set; }
+        public string productCollectionName { get; set; }
+        public string productDetailsCollectionName { get; set; }
+        public string productImageCollectionName { get; set; }
+        public string connectionString { get; set; }
+        public string databaseName { get; set; }
+    }
+}
