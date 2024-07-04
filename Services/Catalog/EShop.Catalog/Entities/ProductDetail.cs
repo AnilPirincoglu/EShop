38969e61c49c5f454d4b466510461227b9605dbc
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-07-04 16:00:40 +0300

    Add ProductDetail entity

---
+++ Services/Catalog/EShop.Catalog/Entities/ProductDetail.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace EShop.Catalog.Entities
+{
+    public class ProductDetail
+    {
+        public string productDetailId { get; set; }
+        public string productDescription { get; set; }
+        public string productInformation { get; set; }
+
+    }
+}
