1cfe016af926462b7944266332bb1689c49f6b67
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-07-04 16:00:54 +0300

    Add ProductImages entity

---
+++ Services/Catalog/EShop.Catalog/Entities/ProductImages.cs
@@ -0,0 +1,12 @@
+﻿namespace EShop.Catalog.Entities
+{
+    public class ProductImages
+    {
+        public string productImageId { get; set; }
+        public string productImage1 { get; set; }
+        public string productImage2 { get; set; }
+        public string productImage3 { get; set; }
+        public string productId { get; set; }
+        public Product product { get; set; }
+    }
+}
