a055b2cba9493c80de388a38662278a77f7af600
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-07-04 16:00:08 +0300

    Add Category entity

---
+++ Services/Catalog/EShop.Catalog/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace EShop.Catalog.Entities
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public  string categoryId { get; set; }
+        public string categoryName { get; set; }    
+    }
+}
