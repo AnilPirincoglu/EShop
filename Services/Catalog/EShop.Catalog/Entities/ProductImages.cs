fb65dcc26e68429af8e17bf83a2be4b2f08d1ed0
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-07-04 16:58:51 +0300

    Refactor ProductImages dependency

--- Services/Catalog/EShop.Catalog/Entities/ProductImages.cs
+++ Services/Catalog/EShop.Catalog/Entities/ProductImages.cs
@@ -1,12 +1,19 @@
-﻿namespace EShop.Catalog.Entities
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace EShop.Catalog.Entities
 {
     public class ProductImages
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string productImageId { get; set; }
         public string productImage1 { get; set; }
         public string productImage2 { get; set; }
         public string productImage3 { get; set; }
         public string productId { get; set; }
+
+        [BsonIgnore]
         public Product product { get; set; }
     }
 }
