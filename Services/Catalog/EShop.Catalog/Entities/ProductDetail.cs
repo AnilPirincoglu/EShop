21f1786aa85d67cc44974e35f9c047f6269996e8
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-07-04 16:56:00 +0300

    Refactor ProductDetail dependency

--- Services/Catalog/EShop.Catalog/Entities/ProductDetail.cs
+++ Services/Catalog/EShop.Catalog/Entities/ProductDetail.cs
@@ -1,12 +1,19 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace EShop.Catalog.Entities
 {
     public class ProductDetail
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string productDetailId { get; set; }
         public string productDescription { get; set; }
         public string productInformation { get; set; }
+        public string productId { get; set; }
+
+        [BsonIgnore]
+        public Product product { get; set; }
 
     }
 }
