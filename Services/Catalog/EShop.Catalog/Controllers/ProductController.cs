e59d527393fd24e99562ff2c6ac6f9cee69ea264
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-07-06 01:45:46 +0300

    Add Product Controller

---
+++ Services/Catalog/EShop.Catalog/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using EShop.Catalog.Dtos.ProductDtos;
+using EShop.Catalog.Services.ProductServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            this._productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var products = await _productService.GetAllProductAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductById(string id)
+        {
+            var product = await _productService.GetByIdProductAsync(id);
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromBody] CreateProductDto createProductDto)
+        {
+            await _productService.CreateProductAsync(createProductDto);
+            return Created();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(string id)
+        {
+            await _productService.DeleteProductAsync(id);
+            return NoContent();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct([FromBody] UpdateProductDto updateProductDto)
+        {
+            await _productService.UpdateProductAsync(updateProductDto);
+            return Ok("Product Updated Successfully");
+        }
+    }
+}
