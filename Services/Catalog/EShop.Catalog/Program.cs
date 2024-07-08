6d16417e2082242a2669e46b0501fab2f5ae664a
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-07-08 15:27:18 +0300

    Add services to the container

--- Services/Catalog/EShop.Catalog/Program.cs
+++ Services/Catalog/EShop.Catalog/Program.cs
@@ -1,6 +1,23 @@
+using EShop.Catalog.Services.CategoryServices;
+using EShop.Catalog.Services.ProductDetailServices;
+using EShop.Catalog.Services.ProductImagesServices;
+using EShop.Catalog.Services.ProductServices;
+using EShop.Catalog.Settings;
+using Microsoft.Extensions.Options;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductDetailService, ProductDetailService>();
+builder.Services.AddScoped<IProductImagesService, ProductImagesService>();
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+builder.Services.AddScoped<IDatabaseSettings>(sp => sp.GetRequiredService<IOptions<DatabaseSettings>>().Value);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
