e09490ab1a6ae25cc300afb1b827f3666a074b56
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-07-12 01:19:13 +0300

    Add Adress, Order and OrderDetail Entities

---
+++ Services/Order/Core/EShop.Order.Domain/Entities/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.Order.Domain.Entities
+{
+    internal class Order
+    {
+        public int OrderId { get; set; }
+        public string UserId { get; set; }
+        public decimal OrderTotalPrice { get; set; }
+        public DateTime OrderDate { get; set; }
+        public Adress Adress { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
+    }
+}
