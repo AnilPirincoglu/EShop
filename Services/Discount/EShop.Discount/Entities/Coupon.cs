cfd18620cb970779f29da0143d3d727fe93548af
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-07-11 11:20:52 +0300

    Add Coupon Entity

---
+++ Services/Discount/EShop.Discount/Entities/Coupon.cs
@@ -0,0 +1,11 @@
+﻿namespace EShop.Discount.Entities
+{
+    public class Coupon
+    {
+        public int couponId { get; set; }
+        public string code { get; set; }
+        public int rate { get; set; }
+        public bool isActive { get; set; }
+        public DateTime validDate { get; set; }
+    }
+}
