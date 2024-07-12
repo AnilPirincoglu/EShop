bb3bcfe30880c285042678983b333c1b1794379d
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-07-12 15:40:22 +0300

    Add OrderDetail Commands

---
+++ Services/Order/Core/EShop.Order.Application/Features/CQRS/Commands/OrderDetailCommands/CreateOrderDetailCommand.cs
@@ -0,0 +1,20 @@
+﻿using EShop.Order.Application.Interfaces;
+using EShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.Order.Application.Features.CQRS.Commands.OrderDetailCommands
+{
+    public class CreateOrderDetailCommand
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int ProductAmount { get; set; }
+        public decimal ProductTotalPrice { get; set; }
+        public int OrderId { get; set; }
+    }
+}
