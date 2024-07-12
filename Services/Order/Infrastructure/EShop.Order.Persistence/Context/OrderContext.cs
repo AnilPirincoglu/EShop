8019c653413e9969cfee77cd4b5b44ca34014293
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-07-12 15:40:00 +0300

    Add OrderContext

---
+++ Services/Order/Infrastructure/EShop.Order.Persistence/Context/OrderContext.cs
@@ -0,0 +1,22 @@
+﻿using EShop.Order.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.Order.Persistence.Context
+{
+    public class OrderContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=ANILPC;initial Catalog=EShopOrderDb;integrated Security=true;TrustServerCertificate=true");
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Ordering> Orders { get; set; }
+    }
+}
