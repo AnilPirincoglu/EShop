a18ea3a735905fe421d05f8e06111d5df1e96390
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-07-18 16:04:53 +0300

    Add Address and OrderDetail services to the container

--- Services/Order/Presentation/EShop.Order.WebApi/Program.cs
+++ Services/Order/Presentation/EShop.Order.WebApi/Program.cs
@@ -1,6 +1,19 @@
+using EShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<GetAddressByIdQueryHandler>();
+builder.Services.AddScoped<GetAddressQueryHandler>();
+builder.Services.AddScoped<CreateAddressCommandHandler>();
+builder.Services.AddScoped<UpdateAddressCommandHandler>();
+builder.Services.AddScoped<DeleteAddressCommandHandler>();
+
+builder.Services.AddScoped<GetAddressByIdQueryHandler>();
+builder.Services.AddScoped<GetAddressQueryHandler>();
+builder.Services.AddScoped<CreateAddressCommandHandler>();
+builder.Services.AddScoped<UpdateAddressCommandHandler>();
+builder.Services.AddScoped<DeleteAddressCommandHandler>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
