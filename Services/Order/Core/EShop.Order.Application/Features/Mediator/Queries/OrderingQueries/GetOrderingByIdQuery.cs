ce5a4d86a318fbb564f3632d8725f2cabc568309
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-07-18 16:05:33 +0300

    Add Ordering Queries

---
+++ Services/Order/Core/EShop.Order.Application/Features/Mediator/Queries/OrderingQueries/GetOrderingByIdQuery.cs
@@ -0,0 +1,20 @@
+﻿using EShop.Order.Application.Features.Mediator.Results.OrderingResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.Order.Application.Features.Mediator.Queries.OrderingQueries
+{
+    public class GetOrderingByIdQuery : IRequest<GetOrderingByIdQueryResult>
+    {
+        public int OrderId { get; set; }
+
+        public GetOrderingByIdQuery(int orderId)
+        {
+            OrderId = orderId;
+        }
+    }
+}
