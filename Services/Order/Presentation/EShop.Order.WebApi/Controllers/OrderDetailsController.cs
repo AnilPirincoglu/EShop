2401c6c383b7934ca3f10e8a04f7bffd361b4f13
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-07-18 16:03:59 +0300

    Add OrderDetails Controller

---
+++ Services/Order/Presentation/EShop.Order.WebApi/Controllers/OrderDetailsController.cs
@@ -0,0 +1,67 @@
+﻿using EShop.Order.Application.Features.CQRS.Commands.OrderDetailCommands;
+using EShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+using EShop.Order.Application.Features.CQRS.Queries.OrderDetailQueries;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Order.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderDetailsController : ControllerBase
+    {
+        private readonly GetOrderDetailQueryHandler _getOrderDetailQueryHandler;
+        private readonly GetOrderDetailByIdQueryHandler _getOrderDetailByIdQueryHandler;
+        private readonly CreateOrderDetailCommandHandler _createOrderDetailCommandHandler;
+        private readonly DeleteOrderDetailCommandHandler _deleteOrderDetailCommandHandler;
+        private readonly UpdateOrderDetailCommandHandler _updateOrderDetailCommandHandler;
+
+        public OrderDetailsController(UpdateOrderDetailCommandHandler updateOrderDetailCommandHandler,
+                                      DeleteOrderDetailCommandHandler deleteOrderDetailCommandHandler,
+                                      CreateOrderDetailCommandHandler createOrderDetailCommandHandler,
+                                      GetOrderDetailByIdQueryHandler getOrderDetailByIdQueryHandler,
+                                      GetOrderDetailQueryHandler getOrderDetailQueryHandler)
+        {
+            _updateOrderDetailCommandHandler = updateOrderDetailCommandHandler;
+            _deleteOrderDetailCommandHandler = deleteOrderDetailCommandHandler;
+            _createOrderDetailCommandHandler = createOrderDetailCommandHandler;
+            _getOrderDetailByIdQueryHandler = getOrderDetailByIdQueryHandler;
+            _getOrderDetailQueryHandler = getOrderDetailQueryHandler;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllOrderDetails()
+        {
+            var result = await _getOrderDetailQueryHandler.Handle();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrderDetailById(int id)
+        {
+            var result = await _getOrderDetailByIdQueryHandler.Handle(new GetOrderDetailByIdQuery(id));
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrderDetail([FromBody] CreateOrderDetailCommand createOrderDetailCommand)
+        {
+            await _createOrderDetailCommandHandler.Handle(createOrderDetailCommand);
+            return Ok("OrderDetail Created Successfuly");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateOrderDetail([FromBody] UpdateOrderDetailCommand updateOrderDetailCommand)
+        {
+            await _updateOrderDetailCommandHandler.Handle(updateOrderDetailCommand);
+            return Ok("OrderDetail Updated Successfuly");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrderDetail(int id)
+        {
+            await _deleteOrderDetailCommandHandler.Handle(new DeleteOrderDetailCommand(id));
+            return Ok("OrderDetail Deleted Successfuly");
+        }
+    }
+}
