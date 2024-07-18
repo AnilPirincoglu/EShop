f371f02e4724e7455f1adfba49a28e99f70e86c5
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-07-18 16:05:16 +0300

    Add Ordering Commands

---
+++ Services/Order/Core/EShop.Order.Application/Features/Mediator/Commands/OrderingCommands/UpdateOrderingCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.Order.Application.Features.Mediator.Commands.OrderingCommands
+{
+    public class UpdateOrderingCommand :IRequest
+    {
+        public int OrderId { get; set; }
+        public string UserId { get; set; }
+        public decimal OrderTotalPrice { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
