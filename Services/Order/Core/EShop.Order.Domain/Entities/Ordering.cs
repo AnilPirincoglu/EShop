f8f7d0d54a4d49fb6aae8858cb26804fb7424ab1
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-07-18 16:02:54 +0300

    Refactore Ordering Entity

--- Services/Order/Core/EShop.Order.Domain/Entities/Ordering.cs
+++ Services/Order/Core/EShop.Order.Domain/Entities/Ordering.cs
@@ -12,7 +12,6 @@ namespace EShop.Order.Domain.Entities
         public string UserId { get; set; }
         public decimal OrderTotalPrice { get; set; }
         public DateTime OrderDate { get; set; }
-        public Address Address { get; set; }
         public List<OrderDetail> OrderDetails { get; set; }
     }
 }
