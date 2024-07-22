21e845c56fc3ac567c4919050f2b1060793f04b0
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-07-22 12:35:16 +0300

    Add Service Registration for Ordering

--- Services/Order/Presentation/EShop.Order.WebApi/Program.cs
+++ Services/Order/Presentation/EShop.Order.WebApi/Program.cs
@@ -1,8 +1,17 @@
 using EShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
+using EShop.Order.Application.Interfaces;
+using EShop.Order.Application.Services;
+using EShop.Order.Persistence.Repositories;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddApplicationService(builder.Configuration);
+
+#region
 builder.Services.AddScoped<GetAddressByIdQueryHandler>();
 builder.Services.AddScoped<GetAddressQueryHandler>();
 builder.Services.AddScoped<CreateAddressCommandHandler>();
@@ -14,6 +23,7 @@ builder.Services.AddScoped<GetAddressQueryHandler>();
 builder.Services.AddScoped<CreateAddressCommandHandler>();
 builder.Services.AddScoped<UpdateAddressCommandHandler>();
 builder.Services.AddScoped<DeleteAddressCommandHandler>();
+#endregion
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
