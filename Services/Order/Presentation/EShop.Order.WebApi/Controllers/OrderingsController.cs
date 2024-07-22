6f4e55ba25180ee0c32fbc9e3a612ba7547b08d2
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-07-22 12:34:04 +0300

    Add Ordering Controller

---
+++ Services/Order/Presentation/EShop.Order.WebApi/Controllers/OrderingsController.cs
@@ -0,0 +1,56 @@
+﻿using EShop.Order.Application.Features.Mediator.Commands.OrderingCommands;
+using EShop.Order.Application.Features.Mediator.Queries.OrderingQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Order.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderingsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrderingsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllOrderings()
+        {
+            var orderings = await _mediator.Send(new GetOrderingQuery());
+            return Ok(orderings);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrderingById(int id)
+        {
+            var ordering = await _mediator.Send(new GetOrderingByIdQuery(id));
+            return Ok(ordering);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrdering([FromBody] CreateOrderingCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Order Created Successfuly");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateOrdering([FromBody] UpdateOrderingCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Order Updated Successfuly");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrdering(int id)
+        {
+            await _mediator.Send(new DeleteOrderingCommand(id));
+            return Ok("Order Deleted Successfuly");
+        }
+
+    }
+}
