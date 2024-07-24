5f8b45fb7802635968d8e03a13a7ca5287644aa5
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-07-24 21:35:13 +0300

    Refactor config class according to Authority

--- IdentityServer/EShop.IdentityServer/Config.cs
+++ IdentityServer/EShop.IdentityServer/Config.cs
@@ -2,57 +2,74 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
+using Microsoft.AspNetCore.Identity;
 using System.Collections.Generic;
 
 namespace EShop.IdentityServer
 {
     public static class Config
     {
-        public static IEnumerable<IdentityResource> IdentityResources =>
-                   new IdentityResource[]
-                   {
-                new IdentityResources.OpenId(),
-                new IdentityResources.Profile(),
-                   };
+        public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
+        {
+           new ApiResource("ResourceCatalog"){ Scopes = { "CatalogFullPermisson","CatalogReadPermisson" }},
+           new ApiResource("ResourceDiscount"){ Scopes = { "DiscountFullPermisson" }},
+           new ApiResource("RessourceOrder"){ Scopes = { "OrderFullPermisson" }}
+        };
 
-        public static IEnumerable<ApiScope> ApiScopes =>
-            new ApiScope[]
+        public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
+        {
+               new IdentityResources.OpenId(),
+               new IdentityResources.Email(),
+               new IdentityResources.Profile()
+        };
+
+        public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
+        {
+             new ApiScope("CatalogFullPermisson","Full authority for catalog operations"),
+             new ApiScope("CatalogReadPermisson","Reading authority for catalog operations"),
+             new ApiScope("DiscountFullPermisson","Full authority for discount operations"),
+             new ApiScope("OrderFullPermisson","Full authority for order operations")
+        };
+
+        public static IEnumerable<Client> Clients => new Client[]
+        {
+            //Visitor
+            new Client()
             {
-                new ApiScope("scope1"),
-                new ApiScope("scope2"),
-            };
+                ClientId = "EShopVisitorId",
+                ClientName = "EShop Visitor User",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = { new Secret("eshopsecret".Sha256()) },
+                AllowedScopes = { "CatalogReadPermisson" }
+            },
 
-        public static IEnumerable<Client> Clients =>
-            new Client[]
+            //Manager
+            new Client()
             {
-                // m2m client credentials flow client
-                new Client
-                {
-                    ClientId = "m2m.client",
-                    ClientName = "Client Credentials Client",
+                ClientId = "EShopManagerId",
+                ClientName = "EShop Manager User",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = { new Secret("eshopsecret".Sha256()) },
+                AllowedScopes = { "CatalogFullPermisson" }
+            },
 
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
-                    AllowedScopes = { "scope1" }
-                },
-
-                // interactive client using code flow + pkce
-                new Client
-                {
-                    ClientId = "interactive",
-                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-
-                    AllowedGrantTypes = GrantTypes.Code,
-
-                    RedirectUris = { "https://localhost:44300/signin-oidc" },
-                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
-                    AllowOfflineAccess = true,
-                    AllowedScopes = { "openid", "profile", "scope2" }
-                },
-            };
+            //Admin
+            new Client()
+            {
+                ClientId = "EShopAdminId",
+                ClientName = "EShop Admin User",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = { new Secret("eshopsecret".Sha256()) },
+                AllowedScopes = { "CatalogFullPermisson","DiscountFullPermisson","OrderFullPermisson"
+                    ,IdentityServerConstants.LocalApi.ScopeName
+                    ,IdentityServerConstants.StandardScopes.Email
+                    ,IdentityServerConstants.StandardScopes.OpenId
+                    ,IdentityServerConstants.StandardScopes.Profile
+                    },
+                AccessTokenLifetime=600
+            }
+        };
     }
 }
